1f942b59d4673f70247399471b05206171669aee
Michał Przyłucki <michal.przyl@gmail.com>
2024-03-15 19:13:54 +0100

    working window on SDL2

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+// #include <SDL/include/SDL.h>
+// #include "SDL/include/SDL"
+
+int main(int argc, char *argv[]){
+    int WIDTH = 400;
+    int HEIGHT = 400;
+    // return 0;
+    SDL_Init( SDL_INIT_EVERYTHING );
+    puts("Starting program...");
+    SDL_Window *window = SDL_CreateWindow( "Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
+    if (window == NULL){
+        puts("cant create window");
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+    while (1)
+    {
+        if(SDL_PollEvent(&windowEvent))
+        {
+            if (windowEvent.type == SDL_QUIT)
+            {break;}
+        }
+    }
+
+    
+    SDL_DestroyWindow( window );
+    SDL_Quit( );
+}
